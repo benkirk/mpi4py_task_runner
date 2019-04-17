0e1cbf78eef475da63d49603e463fd22c5f5e931
Benjamin S. Kirk <benjamin.kirk@nasa.gov>
2019-04-17 17:24:44 -0500

    use the mpi4py lmod module

--- utils/config_env_europa.sh
+++ utils/config_env_europa.sh
@@ -3,25 +3,12 @@ module purge
 module load gcc slurm
 module load anaconda/3-5.0.1
 module load mpt
-export PYTHONPATH=/software/x86_64/mpi4py/3.0.1-${MPI_ID_STRING}/lib/python3.6/site-packages
-
-# # Anaconda 4.2.0; python2
-#  module purge
-#  module load gcc slurm
-#  module load anaconda/4.2.0
-#  module load openmpi/1.10.4
-#  export PYTHONPATH=/software/x86_64/mpi4py/3.0.1-${MPI_ID_STRING}/lib/python2.7/site-packages
-
-# # default (CentOS 7) python2
-# module purge
-# module load gcc slurm
-# module load openmpi/1.10.4
-# export PYTHONPATH=/software/x86_64/mpi4py/3.0.1-${MPI_ID_STRING}/lib/python2.7/site-packages
-
+module load mpi4py/3.0.1-python${PYTHON_VERSION}
 
 
 
 #####################
+echo "PYTHON_VERSION="${PYTHON_VERSION}
 echo $PYTHONPATH
 which mpiexec
 which python
