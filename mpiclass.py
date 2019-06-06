70b6397a3ced54940e7f5802a0dd529df06f7660
Benjamin S. Kirk <benjamin.kirk@nasa.gov>
2019-06-06 15:28:43 -0500

    more tags

--- mpiclass.py
+++ mpiclass.py
@@ -10,11 +10,11 @@ import shutil
 ################################################################################
 class MPIClass:
 
-    tags ={ 'ready'       : 10,
-            'execute'     : 11,
-            'work_assign' : 20,
-            'work_reply'  : 21,
-            'terminate'   : 1000 }
+    tags ={ 'ready'         : 10,
+            'execute'       : 11,
+            'work_reply'    : 20,
+            'work_request'  : 21,
+            'terminate'     : 1000 }
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def __init__(self,options=None,initdirs=True):
