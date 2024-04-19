dced75875e7bc71ee6a0ed997e1ae30d9f984de4
Ben Kirk <benkirk@ucar.edu>
2024-04-19 10:50:38 -0600

    conda envs

---
+++ utils/config_env_casper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+module purge
+module load gcc openmpi conda
+module list
+
+export MPICC=$(which mpicc)
+
+envname="mpi4py-${NCAR_BUILD_ENV}"
+conda activate ${envname} \
+    || conda env create \
+             -f conda.yaml \
+             -n ${envname}
