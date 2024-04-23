68e9c08073973755d01182f69f7869ebb8607b1c
Ben Kirk <benkirk@ucar.edu>
2024-04-22 19:39:19 -0600

    intel option on casper

--- utils/config_env_casper.sh
+++ utils/config_env_casper.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-module purge
-module load gcc openmpi conda
+#module purge
+#module load gcc openmpi conda
+module load conda
 module list
 
 export MPICC=$(which mpicc)
 
 envname="mpi4py-${NCAR_BUILD_ENV}"
 conda activate ${envname} \
-    || conda env create \
-             -f conda.yaml \
-             -n ${envname}
+    || { conda env create -f conda.yaml -n ${envname}; conda activate ${envname}; }
